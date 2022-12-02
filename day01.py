e1142fb125e24015a69f9f6c4f108ad8656aea1f
noob-anmol <anmolghai50@gmail.com>
2022-12-02 23:17:27 +0530

    made changes to day01 solution

--- day01.py
+++ day01.py
@@ -2,22 +2,14 @@ with open("day01_input.txt") as f:
     text = f.read()
     s = text.split("\n")
     first = 0
-    second = 0
-    third = 0
     total = 0
-    elf = 0
     value = []
     for i in s:
         if i == "":
-            elf += 1
             value.append(total)
-            if total > first:
-                third = second
-                second = first
-                first = total
             total = 0
         else:
             total += int(i)
-    print(first, second, third, elf, first + second + third)
     value = sorted(value)
-    print( value[-1] + value[-2] + value[-3])
+    print("The maximum calorie an individual elf is carrying is", value[-1])
+    print("The sum of top 3 calories is", value[-1] + value[-2] + value[-3])
